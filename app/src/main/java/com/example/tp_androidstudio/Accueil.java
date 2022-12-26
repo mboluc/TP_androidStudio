3474929e9c1e74f9fd8e72aee664d021d167ab44
mboluc <mboulanalucie@gmail.com>
2022-12-26 21:08:14 +0100

    ajout3

--- app/src/main/java/com/example/tp_androidstudio/Accueil.java
+++ app/src/main/java/com/example/tp_androidstudio/Accueil.java
@@ -3,6 +3,7 @@ package com.example.tp_androidstudio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Scroller;
 
 public class Accueil extends AppCompatActivity {
 
@@ -11,4 +12,5 @@ public class Accueil extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
     }
+
 }
