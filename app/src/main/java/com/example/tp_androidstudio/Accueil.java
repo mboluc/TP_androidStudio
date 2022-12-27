f522a3f1a34c9654889aaf113674518176423557
mboluc <mboulanalucie@gmail.com>
2022-12-27 18:40:51 +0100

    ajout4

--- app/src/main/java/com/example/tp_androidstudio/Accueil.java
+++ app/src/main/java/com/example/tp_androidstudio/Accueil.java
@@ -1,8 +1,10 @@
 package com.example.tp_androidstudio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.Scroller;
 
 public class Accueil extends AppCompatActivity {
@@ -11,6 +13,32 @@ public class Accueil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+        setSupportActionBar((Toolbar) findViewById(R.id.menu));
+    }
+
+    public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_accueil);
+            //1 - Configuring Toolbar
+            this.configureToolbar();
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            //2 - Inflate the menu and add it to the Toolbar
+            getMenuInflater().inflate(R.menu.activity_accueil);
+            return true;
+        }
+
+        private void configureToolbar(){
+            // Get the toolbar view inside the activity layout
+            Toolbar toolbar = (Toolbar) findViewById(R.id.menu);
+            // Sets the Toolbar
+            setSupportActionBar(toolbar);
+        }
     }
 
 }
